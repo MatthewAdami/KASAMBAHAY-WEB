8434d8ee7ca12482d80822b7f1a23e63cec14bf0
MatthewAdami <132027817+MatthewAdami@users.noreply.github.com>
2026-05-18 11:43:37 +0800

    chore: clean up vite config for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // 💡 Back to BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 💡 ADDED THE BASENAME PARAMETER HERE */}
-    <BrowserRouter basename="/KASAMBAHAY-WEB">
+    <BrowserRouter> {/* 💡 Clean wrapper, no basename needed! */}
       <App />
     </BrowserRouter>
   </StrictMode>,
